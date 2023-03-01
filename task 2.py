b9eae9c0b44fda19914cbe0a50438657897f2fcf
steeluv <124870006+steeluv@users.noreply.github.com>
2023-03-01 11:16:23 +0300

    Add files via upload

--- task 2.py
+++ task 2.py
@@ -1,8 +1,10 @@
-hours = int(input())
-price = int(input())
-if hours >= 10 and hours <=12:
-    print(price/2)
-elif hours >=20 and hours <=22:
-    print(price/4)
-else:
-    print(price)
+marks = [5,3,4,2]
+mark = len(marks)
+print(mark)
+a = 0
+for i in marks:
+    if 3 <= i <= 5:
+        a += 1
+print(a/mark*100)
+
+
--- task 4.py
+++ task 4.py
@@ -1,12 +1,13 @@
-category = input('Категория товара ')
-if category == 'продукты':
-    price = int(input())
-    if price < 100:
-        print('Попробуйте нашу выпечку!')
-    if price >= 100 and price < 500:
-        print('Как начёт орехов в шоколаде?')
-    if price >=500:
-        print('Попоробуйте экзотические фрукты!')
-else:
-    print('Загляните в товары для дома!')
+teachers = []
 
+a = input('Добавить информацию о новом наставнике(да/нет)').lower()
+
+while a !='нет':
+    teacher = []
+    if a =='да':
+        teacher.append(input('Фамилия,Имя наставника:'))
+        teacher.append(input('Должность: '))
+        teacher.append(input('Количество студентов во всех группах:'))
+        teachers.append(teacher)
+    print(teachers)
+    a = input('Записать нового наставника?(да/нет)').lower()
